ee8f21571f30bcb34699bf4d3f54a3dc41f5b0a0
supermaciu <m.jankowski2605@gmail.com>
2024-09-06 21:43:57 +0200

    mouse rotation: kinda finished

--- Mouse Rotation/main.py
+++ Mouse Rotation/main.py
@@ -17,15 +17,16 @@ running = True
 dt = 0
 
 class Player:
-    MOVEMENT_SPEED = 200
-    ROTATION_SPEED = 1
+    MOVEMENT_SPEED = 150
     
     def __init__(self):
         self.points = [
             (PLAYER_STARTING_X, PLAYER_STARTING_Y), 
-            (PLAYER_STARTING_X-30, PLAYER_STARTING_Y-30),
-            (PLAYER_STARTING_X+30, PLAYER_STARTING_Y-30)
+            (PLAYER_STARTING_X-40, PLAYER_STARTING_Y+10),
+            (PLAYER_STARTING_X-40, PLAYER_STARTING_Y-10)
         ]
+
+        self.oldAngle = 0
     
     def move(self, axis, speed, dt):
         for i in range(len(self.points)):
@@ -36,31 +37,51 @@ class Player:
                 py += speed * dt
             self.points[i] = (px, py)
 
-    def rotate(self, mouse, dt):
+    def rotate(self, mouse):
 
         # Calculating the angle
         ox, oy = self.points[0] # origin
         mx, my = mouse
 
-        angle = math.atan((my - oy)/(mx - ox))
-        print(f"{(mx - ox)=} {(my - oy)=} {(my - oy)/(mx - ox)=} {(angle)=}")
+        if (mx - ox) != 0:
+            angle = math.atan((my - oy)/(mx - ox))
+        else:
+            angle = 0
+
+        # Angle quarters
+        if (mx - ox) > 0 and (my - oy) > 0:
+            pass
+        elif (mx - ox) < 0 and (my - oy) > 0:
+            angle += math.pi
+        elif (mx - ox) < 0 and (my - oy) < 0:
+            angle += math.pi
+        elif (mx - ox) > 0 and (my - oy) < 0:
+            angle += 2 * math.pi
+
+        temp = angle
+        angle -= self.oldAngle # new angle - old angle = difference/delta
+        self.oldAngle = temp
         
         for i in range(1, len(self.points)): # without origin point
             px, py = self.points[i]
 
+            # normal coordinate system
+            # new_px = (px - ox) * math.cos(angle) - (py - oy) * math.sin(angle) + ox
+            # new_py = (py - oy) * math.cos(angle) + (px - ox) * math.sin(angle) + oy
+            
+            # x increasing from left to right and y increasing from top to bottom
             new_px = (px - ox) * math.cos(angle) - (py - oy) * math.sin(angle) + ox
-            new_py = (py - oy) * math.cos(angle) + (px - ox) * math.sin(angle) + oy
+            new_py = (px - ox) * math.sin(angle) + (py - oy) * math.cos(angle) + oy
 
             self.points[i] = (new_px, new_py)
 
     def update(self, dt):
-        pass
+        self.rotate(pygame.mouse.get_pos())
 
     def draw(self):
         # for point in self.points:
         #     pygame.draw.circle(screen, WHITE, point, 5)
         pygame.draw.polygon(screen, WHITE, self.points)
-        pygame.draw.circle(screen, RED, self.points[0], 10)
 
 player = Player()
 
@@ -69,10 +90,15 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-        # One pressed event
+        # One pressed event keys
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_e:
-                player.rotate(pygame.mouse.get_pos(), 0)
+                print("test")
+
+        # One pressed event mouse
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == pygame.BUTTON_LEFT:
+                print("test")
 
     keys = pygame.key.get_pressed()
 
@@ -93,6 +119,6 @@ while running:
 
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(120) / 1000
 
 pygame.quit()
