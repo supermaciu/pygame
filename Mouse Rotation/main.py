455d56aaf589ed7d82e2fcd1f45f1e4e690d3d11
supermaciu <m.jankowski2605@gmail.com>
2025-02-24 17:11:06 +0100

    changes

--- Mouse Rotation/main.py
+++ Mouse Rotation/main.py
@@ -2,6 +2,7 @@ import pygame, math
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+GRAY = (130, 130, 130)
 RED = (200, 0, 0)
 
 SCREEN_WIDTH = 600
@@ -18,6 +19,7 @@ dt = 0
 
 class Player:
     MOVEMENT_SPEED = 150
+    BULLET_SPEED = 10
     
     def __init__(self):
         self.points = [
@@ -25,8 +27,23 @@ class Player:
             (PLAYER_STARTING_X-40, PLAYER_STARTING_Y+10),
             (PLAYER_STARTING_X-40, PLAYER_STARTING_Y-10)
         ]
-
+        self.playerCentroid = Player.calculate_centroid(self.points)
         self.oldAngle = 0
+
+        self.bullet = [
+            (0, 0), 
+            (-20, 5),
+            (-20, -5)
+        ]
+        self.bulletCentroid = Player.calculate_centroid(self.bullet)
+        self.bullets = list()
+
+    @classmethod
+    def calculate_centroid(cls, points):
+        return (
+            sum(list(point)[0] for point in points) / len(points),
+            sum(list(point)[1] for point in points) / len(points),
+        )
     
     def move(self, axis, speed, dt):
         for i in range(len(self.points)):
@@ -37,10 +54,17 @@ class Player:
                 py += speed * dt
             self.points[i] = (px, py)
 
+        cx, cy = self.playerCentroid
+        if axis == 0:
+            cx += speed * dt
+        elif axis == 1:
+            cy += speed * dt
+        self.playerCentroid = (cx, cy)
+
     def rotate(self, mouse):
 
         # Calculating the angle
-        ox, oy = self.points[0] # origin
+        ox, oy = self.playerCentroid
         mx, my = mouse
 
         if (mx - ox) != 0:
@@ -48,7 +72,7 @@ class Player:
         else:
             angle = 0
 
-        # Angle quarters
+        # Angle converted to full angle
         if (mx - ox) > 0 and (my - oy) > 0:
             pass
         elif (mx - ox) < 0 and (my - oy) > 0:
@@ -58,11 +82,21 @@ class Player:
         elif (mx - ox) > 0 and (my - oy) < 0:
             angle += 2 * math.pi
 
+        # Tangens angle excepitons
+        if (mx - ox) > 0 and (my - oy) == 0:
+            pass
+        elif (mx - ox) == 0 and (my - oy) < 0:
+            angle += math.pi * 3/2
+        elif (mx - ox) < 0 and (my - oy) == 0:
+            angle += math.pi
+        elif (mx - ox) == 0 and (my - oy) > 0:
+            angle += math.pi / 2
+
         temp = angle
         angle -= self.oldAngle # new angle - old angle = difference/delta
         self.oldAngle = temp
         
-        for i in range(1, len(self.points)): # without origin point
+        for i in range(len(self.points)): # without origin point
             px, py = self.points[i]
 
             # normal coordinate system
@@ -74,14 +108,29 @@ class Player:
             new_py = (px - ox) * math.sin(angle) + (py - oy) * math.cos(angle) + oy
 
             self.points[i] = (new_px, new_py)
+        
+    def fire(self, speed):
+        # bullet = Bullet()
+
+        # cx, cy = self.playerCentroid
+        # bullet = self.bullet
+        # bx, by = bullet
+        # bullet = (bx + cx, by + cy)
+
+        pass
+
+
 
     def update(self, dt):
         self.rotate(pygame.mouse.get_pos())
 
     def draw(self):
-        # for point in self.points:
-        #     pygame.draw.circle(screen, WHITE, point, 5)
+        # player
         pygame.draw.polygon(screen, WHITE, self.points)
+        pygame.draw.circle(screen, GRAY, self.playerCentroid, 2)
+
+        # bullets
+
 
 player = Player()
 
@@ -98,7 +147,8 @@ while running:
         # One pressed event mouse
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == pygame.BUTTON_LEFT:
-                print("test")
+                # player.fire()
+                pass
 
     keys = pygame.key.get_pressed()
 
